afdb074461014814ad9e7dbc65eeea8dbbeea5a8
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2026-05-07 14:16:09 +0300

    feat: project setup

---
+++ main.py
@@ -0,0 +1,13 @@
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+import turtle
+
+one = turtle.Turtle()
+forward = 30
+deg = 60
+
+for color in colors:
+    one.forward(forward)
+    one.right(deg)
+    
+
+# ваш код
